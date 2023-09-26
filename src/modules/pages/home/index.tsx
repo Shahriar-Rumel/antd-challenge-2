f084d0811639dd1d14e4274248892e2f39e10dca
Shahriar Rumel <rumelshahriarhere@gmail.com>
2023-09-27 00:45:45 +0600

    feat: add user search with filter

--- src/modules/pages/home/index.tsx
+++ src/modules/pages/home/index.tsx
@@ -1,17 +1,37 @@
 import MainLayout from '../../layout/MainLayout';
 import Collapsible from '../../shared/Collapsible';
+import Input from '../../shared/Input';
+import { SearchOutlined, FilterOutlined } from '@ant-design/icons';
+import { Button } from 'antd';
+
+const SearchWithFilter = () => {
+  return (
+    <div className="flex gap-4 items-center">
+      <Input
+        placeholder="Search by name, edu, exp or #tag"
+        prefixIcon={<SearchOutlined className="text-[20px] text-slate-400" />}
+        title="Extra information"
+      />
+      <Button
+        icon={<FilterOutlined className="text-slate-500 text-[20px]" />}
+        className="bg-white border-0 h-[50px]"
+      />
+    </div>
+  );
+};
 
 const Home = (props: any) => {
   return (
     <MainLayout>
-      <div className={styles.wrapper}>
+      <section className={styles.wrapper}>
         <div className="flex gap-6">
-          <div className="w-[35%]">
+          <aside className="w-[35%]">
             <Collapsible />
-          </div>
+            <SearchWithFilter />
+          </aside>
           <div className="w-[65%]"></div>
         </div>
-      </div>
+      </section>
     </MainLayout>
   );
 };
